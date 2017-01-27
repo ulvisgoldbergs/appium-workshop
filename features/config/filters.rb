fa912d24b05ab59dff514aca843656223a4adfea
ulvisgoldbergs <ulvis.goldbergs@testdevlab.com>
2017-01-27 12:23:24 +0200

    Update from lecture 5

--- features/config/filters.rb
+++ features/config/filters.rb
@@ -1,30 +1,35 @@
 require 'yaml'
 class Filter
-  @parameters = []
+  attr_accessor :name, :category, :sub_categories, :parameters
 
   def initialize(type)
-    config = YAML.load_file('filters.yml')[type]
+    config = YAML.load_file('features/config/filters.yml')[type]
     @name = config['name']
     @category = config['category']
     @sub_categories = []
     config['sub_categories'].each do |sub_cat|
-      temp_hash = {'title' => sub_cat['sub_category'].first['title'], 'option' => sub_cat['sub_category'].last['option']}
-      @sub_categories.push temp_hash
+      @sub_categories.push sub_cat
     end
-
+    @parameters = []
+    config['parameters'].each do |parameter|
+      @parameters.push parameter
+    end
   end
 
   def print_properties
     print 'Name:' + @name
     print "\nCategory:" + @category
     print "\nSub Categories:"
-    # p @sub_categories
     @sub_categories.each do |sub_cat|
       print "\n\t - " + sub_cat['title'] + ' ' + sub_cat['option']
     end
+    print "\nParameters:"
+    @parameters.each do |parameter|
+      print "\n\t - " + parameter['name'] + ' ' + parameter['left'] + ' ' + parameter['right']
+    end
+    print "\n\n"
   end
 end
-
 filter = Filter.new('property_positive')
 
 filter.print_properties
--- features/tests/test_add_filter.rb
+++ features/tests/test_add_filter.rb
@@ -1,45 +1,55 @@
+require_relative '../config/filters'
 class TestAddFilter
   attr_accessor :driver
   def initialize(screens)
     @screens = screens
+    @filter_data = Filter.new('property_positive')
   end
 
-  def select_category(category)
+  def select_category
     @screens.screen_create_filter.visible?
-    @screens.screen_create_filter.select_row category
+    @screens.screen_create_filter.select_row(@filter_data.category)
   end
 
-  def select_type
-    @screens.screen_select_sub_category.visible?('Nekustamie īpašumi')
-    @screens.screen_select_sub_category.select_sub_category 'Zeme'
+  def select_sub_category(sub_cat_hash)
+    @screens.screen_select_sub_category.visible?(sub_cat_hash['title'])
+    @screens.screen_select_sub_category.select_sub_category(
+      sub_cat_hash['option']
+    )
   end
 
-  def select_district
-    @screens.screen_select_sub_category.visible?('Zeme')
-    @screens.screen_select_sub_category.select_sub_category 'Ventspils un rajons'
+  def open_filter_parameter_screen
+    select_category
+    @filter_data.sub_categories.each do |sub_category|
+      select_sub_category(sub_category)
+    end
   end
 
-  def select_town
-    @screens.screen_select_sub_category.visible?('Ventspils un rajons')
-    @screens.screen_select_sub_category.select_sub_category 'Visi'
+  def set_filter_name
+    @screens.screen_set_filter_parameters.visible?
+    @screens.screen_set_filter_parameters.set_name(@filter_data.name)
   end
 
-  def select_action
-    @screens.screen_select_sub_category.visible?('Darbība')
-    @screens.screen_select_sub_category.select_sub_category 'Visi'
+  def set_all_parameters
+    @screens.screen_set_filter_parameters.visible?
+    @filter_data.parameters.each do |parameter|
+      @screens.screen_set_filter_parameters.set_parameter(parameter)
+    end
   end
+
+    def submit_filter_data
+      set_filter_name
+      set_all_parameters
+      @screens.screen_set_filter_parameters.save_filter
+    end
 
   def submit_empty_filter_parameters
     @screens.screen_set_filter_parameters.visible?
     @screens.screen_set_filter_parameters.save_filter
   end
 
-  def create_empty_filter
-    select_category 'Nekustamie īpašumi'
-    select_type
-    select_district
-    select_town
-    select_action
-    submit_empty_filter_parameters
+  def create_filter
+    open_filter_parameter_screen
+    submit_filter_data
   end
 end
